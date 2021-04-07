131f2a1dd73db2b16f9f01a5d152b8db2da9c42d
Yuki Onishi <yuki.st16@gmail.com>
2021-04-07 12:13:29 +0900

    Separate polygon utilities from polygon_filter

---
+++ src/polygon_utils.h
@@ -0,0 +1,19 @@
+#ifndef POLYGON_UTILS_H
+#define POLYGON_UTILS_H
+
+#include <string>
+
+#include <geometry_msgs/Polygon.h>
+#include <ros/ros.h>
+
+inline double sign0(double x);
+void padPolygon(geometry_msgs::Polygon& polygon, double padding);
+double getNumberFromXMLRPC(XmlRpc::XmlRpcValue& value, const std::string& full_param_name);
+geometry_msgs::Polygon makePolygonFromXMLRPC(const XmlRpc::XmlRpcValue& polygon_xmlrpc,
+                                             const std::string& full_param_name);
+std::vector<std::vector<float> > parseVVF(const std::string& input, std::string& error_return);
+geometry_msgs::Polygon makePolygonFromString(const std::string& polygon_string,
+                                             const geometry_msgs::Polygon& last_polygon);
+std::string polygonToString(geometry_msgs::Polygon polygon);
+
+#endif  // POLYGON_UTILS_H
