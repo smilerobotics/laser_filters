d689afaa7d1749d1b9af65185cbd68caeb8b455f
Yuki Onishi <yuki.st16@gmail.com>
2021-04-14 12:17:57 +0900

    Add Box struct to represent a cuboid

---
+++ src/box.h
@@ -0,0 +1,19 @@
+#ifndef BOX_H
+#define BOX_H
+
+#include <geometry_msgs/Point32.h>
+
+struct Box
+{
+  // A box (cuboid) in without rotation can be described by two different points
+  // on a Cartesian coordinate system, as shown in the following figure.
+  //
+  //               .---. max
+  //              /   /|
+  // z^  y       .---. .
+  //  |/         |   |/
+  //  .-> x  min .---.
+  geometry_msgs::Point32 min, max;
+};
+
+#endif
